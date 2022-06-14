26661cc973668ce10af3c11f1efdedde0605860b
Olyazarichna <olya.zarichna@gmail.com>
2022-06-14 12:08:07 +0300

    library button
    стилізація кнопок, зміна фону при кліку на бібліотеку

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,7 @@ const refs = {
   homeEl: document.querySelector('.header-refs'),
   libraryEl: document.querySelector('.refs-library'),
   formEl: document.querySelector('.search-form'),
+  headerEl: document.querySelector('header'),
 };
 
 refs.libraryEl.addEventListener('click', onLibraryClick);
@@ -11,10 +12,14 @@ function onLibraryClick(event) {
   refs.homeEl.classList.remove('active');
   refs.libraryEl.classList.add('active');
   changeMarkup();
+  changeBackgroundImg();
 }
 function changeMarkup() {
   const markup = `<ul class="list-btn">
  <li class="list-btn-item"><button type="button" class="btn">Watched</button></li>
  <li class="list-btn-item"><button type="button" class="btn">queue</button></li>`;
   refs.formEl.innerHTML = markup;
+}
+function changeBackgroundImg() {
+  refs.headerEl.classList.add('header-library');
 }
