1bbda83df582133f24fd171e94c54625f4fda9d9
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-06-17 00:45:07 +0300

    +add-hover&data-id

--- src/js/renderTrendMovies.js
+++ src/js/renderTrendMovies.js
@@ -21,7 +21,7 @@ apiService
     });
         
       let item =`<li class="movie_card">
-        <img src="https://image.tmdb.org/t/p/original${element.poster_path}" alt="${element.title}" loading="lazy" class='list__img' />
+        <img src="https://image.tmdb.org/t/p/original${element.poster_path}" alt="${element.title}" loading="lazy" data-id = ${element.id} class="list__img"/>
         
         <p class="info_title">${element.original_title}
           <span class="info_genre">${genreStr} | ${element.release_date}</span> 
@@ -34,3 +34,4 @@ apiService
     
     
 }
+
