d8e93550a05043d0b8b7ba1058bc15897694175e
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-06-18 10:39:55 +0300

    added new styles

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,28 +1,36 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
+import './apiServices';
+import ApiService from './apiServices';
+const apiService = new ApiService();
+console.log(apiService);
+
+
 export function makePagination() {
     const pagination = new Pagination('pagination', {
-        totalItems: 5000,
-        itemsPerPage: 10,
-        visiblePages: 5,
-        page: 1,
-        usageStatistics: false,
-        template: {
-            page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-            currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-            moveButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</a>',
-            disabledMoveButton:
-                '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</span>',
-            moreButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                '<span class="tui-ico-ellip">...</span>' +
-                '</a>',
-        },
+  totalItems: 5000,
+  itemsPerPage: 1,
+  visiblePages: 7,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}" >' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>'
+  }
     });
 }
